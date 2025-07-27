2b42eb4ab613c75b66e1f27ec5b0393e390c30d4
Moteen Raza <moteenraza@gmail.com>
2025-07-27 17:28:58 -0400

    Finised the mouse hovering effect in javascript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// create an event listener for mouse events
+var hover = document.getElementById("hover");
+
+hover.addEventListener("mouseenter", function () {
+  this.style.backgroundColor = "skyblue";
+  this.textContent = "welcome back";
+});
+
+hover.addEventListener("mouseleave", function () {
+  this.style.backgroundColor = "gray";
+  this.textContent = "you have left";
+});
 // create an event listener that will listen for the users key press and display it
 const input = document.getElementById("keyInput");
 const output = document.getElementById("lastKey");
