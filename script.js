f2e19cf373c7fae69eb4f6647c98a49daf6f1751
Moteen Raza <moteenraza@gmail.com>
2025-07-27 20:26:07 -0400

    Completed the form section, displaying message when form is submitted

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ const output = document.getElementById("lastKey");
 input.addEventListener("keypress", (event) => {
   output.textContent = event.key;
 });
+
+// create event listener for form on submit
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const username = document.getElementById("name");
+  const message = document.getElementById("message");
+
+  // form submission
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    nameInput = username.value.trim();
+    if (nameInput) {
+      message.textContent = "Your form was submitted, thank you!";
+    } else {
+      message.textContent = "Please enter your name";
+    }
+  });
+});
